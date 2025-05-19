237ab3c9b5056c3f897a0df61f286404ba211b43
Kartik Sanil <22kartiksanil@gmail.com>
2025-05-19 23:08:37 +0400

    trying a new setup
    previous setup is in nvim2.bak

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,59 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- sourcing
+keymap.set("n", "<leader>so", "<cmd>source %<CR>", { desc = "Source current file" })
+keymap.set("n", "<leader>sO", "<cmd>source $MYVIMRC<CR>", { desc = "Source Neovim config" })
+
+-- Save file
+keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "󰆓 Save file" })
+
+-- Quit current window
+keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = " Quit window" })
+
+-- Force quit (without saving)
+keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = " Force quit" })
+
+-- Save and quit current window
+keymap.set("n", "<leader>x", "<cmd>wq<CR>", { desc = " Save & quit" })
+
+-- Save all buffers
+keymap.set("n", "<leader>W", "<cmd>wa<CR>", { desc = "󰅒 Save all buffers" })
+
+-- Save all and quit
+keymap.set("n", "<leader>X", "<cmd>wqa<CR>", { desc = "󰩈 Save all & quit" })
+
+-- split navigation with leader key
+keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Move to left split" })
+keymap.set("n", "<leader>sj", "<C-w>j", { desc = "Move to below split" })
+keymap.set("n", "<leader>sk", "<C-w>k", { desc = "Move to above split" })
+keymap.set("n", "<leader>sl", "<C-w>l", { desc = "Move to right split" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "sc", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- tmux navigation
+keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,106 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- 🗂️ Tree-style file view
+vim.cmd("let g:netrw_liststyle = 3")
+
+-- 🔑 Leader key
+vim.g.mapleader = " "
+
+-- 🌐 Encoding
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- 🔢 Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- 📝 Tabs & Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+
+-- 📜 Wrapping
+vim.opt.wrap = false
+
+-- 🔍 Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.inccommand = "split"
+
+-- 💡 UI & Cursor
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.showmatch = true
+vim.opt.showmode = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = vim.fn.has("nvim-0.8") == 1 and 0 or 1
+vim.opt.laststatus = 3
+vim.opt.scrolloff = 10
+vim.opt.winbar = "%=%m %f"
+
+-- 🎨 Colors
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- 🔙 Backspace
+vim.opt.backspace = { "start", "eol", "indent" }
+
+-- 📋 Clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- 🪟 Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.splitkeep = "cursor"
+
+-- 🖱️ Mouse (disabled for keyboard-only users; change to 'a' if needed)
+vim.opt.mouse = "a"
+
+-- 🧠 Completion Timing
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+
+-- 💾 Swap & Undo
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.backup = false
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+
+-- 🧭 Shell
+vim.opt.shell = "zsh"
+
+-- 🧠 Wildignore & Path
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
+
+-- 🧙 Persistent Sessions
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- ✨ Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- 🟨 Comment formatting
+vim.opt.formatoptions:append({ "r" })
+
+-- 📁 Filetype Detection
+vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+
+vim.filetype.add({
+  extension = {
+    mdx = "mdx",
+  },
+})
+
+-- ⚙️ LazyVim globals (optional)
+vim.g.lazyvim_prettier_needs_config = true
+vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_cmp = "blink.cmp"
